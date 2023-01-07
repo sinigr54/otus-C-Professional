830f8d934a4665459eb43c48d4d4042642fe651b
Alexander Chekh <heapify@yandex.ru>
2023-01-07 22:06:54 +0600

    Fix build

--- tests.cpp
+++ tests.cpp
@@ -2,7 +2,7 @@
 // Created by sinigr on 07.01.2023.
 //
 #include <gtest/gtest.h>
-#include "../lib.h"
+#include "lib.h"
 
 TEST(TestVersion, TestValidVersionGeneration) {
     EXPECT_EQ(version() > 0, true);

6bde5f8214510b71a5f5c88d8d1db1057d2e07e5
Alexander Chekh <heapify@yandex.ru>
2023-01-13 21:38:55 +0600

    Added comments

--- homework_2/main.cpp
+++ homework_2/main.cpp
@@ -8,14 +8,18 @@
 
 int main(int, char **) {
     try {
+        // чтение из файла
         auto readAddresses = homework_2::IPAddressReader::readIPAddresses("ip_filter.tsv", '\n');
 
+        // обратная лексикографическая сортировка
         std::sort(readAddresses.rbegin(), readAddresses.rend());
 
+        // вывод в cout отсортированного списка адресов
         std::copy(readAddresses.begin(),
                   readAddresses.end(),
                   std::ostream_iterator<homework_2::IPAddress>(std::cout, "\n"));
 
+        // вывод списка адресов в которых первый байт равен 1
         std::for_each(readAddresses.begin(), readAddresses.end(), [](const auto &ipAddress) {
             const auto& bytes = ipAddress.getBytes();
             if (!bytes.empty() && bytes[0] == 1)
@@ -24,6 +28,7 @@ int main(int, char **) {
             }
         });
 
+        // вывод списка адресов в которых первый байт равен 46, а второй 70
         std::for_each(readAddresses.begin(), readAddresses.end(), [](const auto &ipAddress) {
             const auto& bytes = ipAddress.getBytes();
             if (bytes.size() > 1 && bytes[0] == 46 && bytes[1] == 70)
@@ -32,6 +37,7 @@ int main(int, char **) {
             }
         });
 
+        // вывод списка адресов в которых есть байт равный 46
         std::for_each(readAddresses.begin(), readAddresses.end(), [](const auto &ipAddress) {
             const std::vector<int>& bytes = ipAddress.getBytes();
             if (!bytes.empty() && std::find(bytes.begin(), bytes.end(), 46) != bytes.end())
